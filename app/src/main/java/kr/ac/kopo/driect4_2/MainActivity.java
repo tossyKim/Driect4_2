64ca261b6e0a8c305a2bd3d61724420fbe464691
tossyKim <dydrkfk22@gmail.com>
2025-03-31 14:47:50 +0900

    Actionbar에 아이콘 추가

--- app/src/main/java/kr/ac/kopo/driect4_2/MainActivity.java
+++ app/src/main/java/kr/ac/kopo/driect4_2/MainActivity.java
@@ -20,5 +20,7 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        getSupportActionBar().setIcon(R.drawable.android14icon);
     }
 }
